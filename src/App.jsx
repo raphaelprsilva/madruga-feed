a77525cf6ba7a778bd0fbdcf8c6f8b73879c7ab2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-20 11:05:31 -0300

    refactor: set posts data as array

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,45 @@ import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        'https://f.i.uol.com.br/fotografia/2019/05/30/15592342355cf006bb66026_1559234235_1x1_md.jpg',
+      name: 'Ramon Valdez',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala turma!' },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projetinho no meu repo.',
+      },
+      { type: 'link', content: 'github.com/ramon' },
+    ],
+    publishedAt: new Date('2023-03-19 12:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        'https://images.unsplash.com/photo-1600697394936-59934aa5951f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+      name: 'Juan Martinez',
+      role: 'Web Designer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala turma!' },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projetinho no meu repo.',
+      },
+      { type: 'link', content: 'github.com/juan' },
+    ],
+    publishedAt: new Date('2023-03-10 12:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -11,14 +50,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Raphael"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni voluptates neque quia fugiat, ex quos cumque, dolorem ut laudantium quidem deserunt. Molestias consequuntur, facere laborum placeat iusto accusantium quo."
-          />
-          <Post
-            author="Rodrigo"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni voluptates neque quia fugiat, ex quos cumque, dolorem ut laudantium quidem deserunt. Molestias consequuntur, facere laborum placeat iusto accusantium quo."
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
