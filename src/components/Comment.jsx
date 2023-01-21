003ccc6aaccde1bacba636f783a445d348dd035a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-21 13:37:23 -0300

    feat: ensures likes can be updated

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './Comment.module.css';
+import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 
+import styles from './Comment.module.css';
+
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1);
+  }
+
   function handleDeleteComment() {
     onDeleteComment(content);
   }
@@ -35,9 +44,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
